2d80a8905edae3e35ab13049370ff0f01343283b
SemyonRubtsov <semarumov@gmail.com>
2024-04-15 20:52:31 +0700

    Реализация лаб №3 для платы

---
+++ project_1.srcs/constrs_1/new/lab3_traffic.xdc
@@ -0,0 +1,14 @@
+set_property -dict {PACKAGE_PIN E15 IOSTANDARD LVCMOS25} [get_ports {o_La[0]}]
+set_property -dict {PACKAGE_PIN D15 IOSTANDARD LVCMOS25} [get_ports {o_La[1]}]
+set_property -dict {PACKAGE_PIN W17 IOSTANDARD LVCMOS25} [get_ports {o_La[2]}]
+#set_property -dict {PACKAGE_PIN W5 IOSTANDARD LVCMOS25} [get_ports {o_led[3]}]
+
+set_property -dict {PACKAGE_PIN V7 IOSTANDARD LVCMOS25} [get_ports {o_Lb[4]}]
+set_property -dict {PACKAGE_PIN W10 IOSTANDARD LVCMOS25} [get_ports {o_Lb[5]}]
+set_property -dict {PACKAGE_PIN P18 IOSTANDARD LVCMOS25} [get_ports {o_Lb[6]}]
+#set_property -dict {PACKAGE_PIN P17 IOSTANDARD LVCMOS25} [get_ports {o_led[7]}]
+
+set_property -dict {PACKAGE_PIN F19 IOSTANDARD LVCMOS25} [get_ports i_rst]
+
+set_property -dict {PACKAGE_PIN W6 IOSTANDARD LVCMOS25} [get_ports i_Ta]
+set_property -dict {PACKAGE_PIN W7 IOSTANDARD LVCMOS25} [get_ports i_Tb]
---
+++ project_1.srcs/sources_1/new/lab3_traffic_top.sv
@@ -0,0 +1,58 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/15/2024 05:40:16 PM
+// Design Name: 
+// Module Name: lab3_top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+
+module lab3_traffic_top(
+    input wire clk_in1_p,
+    input wire clk_in1_n,
+    input i_rst,
+    input i_Ta,
+    input i_Tb,
+    output reg [2:0] o_La,
+    output reg [2:0] o_Lb
+    );
+    
+    wire m_clk;
+    
+    clk_wiz_0 u_pll
+   (
+    // Clock out ports
+    .clk_out1(m_clk),     // output clk_out1
+    // Status and control signals
+    //.reset(i_rst), // input reset
+    .locked(locked),       // output locked
+   // Clock in ports
+    .clk_in1_p(clk_in1_p),    // input clk_in1_p
+    .clk_in1_n(clk_in1_n)
+    );    // input clk_in1_n
+    
+    lab3_top u_sm(
+    .i_clk(m_clk),
+    .i_rst(i_rst),
+    .i_Ta(i_Ta),
+    .i_Tb(i_Tb),
+    .o_La(o_La),
+    .o_Lb(o_Lb)
+    );
+    
+    
+endmodule
