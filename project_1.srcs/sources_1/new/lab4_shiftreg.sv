3c09d6f6fd013e94c8e5a2c295e9f3b7762cee90
SemyonRubtsov <semarumov@gmail.com>
2024-05-02 23:37:45 +0700

    использование shiftreg в source
    использование lab4_shiftreg в lab4_source чтобы получать tvalid без разрывов, не закончено, некорректное начало tvalid

--- project_1.srcs/sources_1/new/lab4_shiftreg.sv
+++ project_1.srcs/sources_1/new/lab4_shiftreg.sv
@@ -20,20 +20,43 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module lab4_shiftreg(
-    input byte i_data,
+module lab4_shiftreg
+#(
+    parameter PACKET_LEN = 12
+)
+(
     input logic i_clk,
+    input byte i_data,
     input logic i_reg_vld,
+    output logic o_data_vld,
     output byte o_data
-    );
+);
     
-    parameter shift = 12;
+    //parameter shift = 12;
 
-    reg [shift-1:0][8-1:0] m_shiftreg = {shift{1'b0}};
+    reg [0:3] m_cnt = '0;
+    reg [PACKET_LEN-1:0][8-1:0] m_shiftreg = {PACKET_LEN{1'b0}};
 
-    always @(posedge i_clk & i_reg_vld)
-        m_shiftreg  <= {i_data, m_shiftreg[shift-1:1]};
-
+    always @(posedge i_clk) begin
+        if (i_reg_vld) begin
+        m_shiftreg  <= {i_data, m_shiftreg[PACKET_LEN-1:1]};
+        
+        if (m_cnt==PACKET_LEN-2)
+            o_data_vld<='0;
+        
+        if (m_cnt==1) o_data_vld<='1; // temporary, must be fixed
+        
+        if (m_cnt<PACKET_LEN-1)
+            m_cnt<=m_cnt+1;
+        else begin
+            m_cnt<=0;
+            //o_data_vld<='1;
+        end
+        
+        end
+        
+    end
+    
     assign o_data = m_shiftreg[0];
     
 endmodule
